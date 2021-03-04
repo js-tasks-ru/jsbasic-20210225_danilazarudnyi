01f06cf4811ab056aa4ebc2e812d0e4cf40cfd33
Lurker10 <Lurker10@users.noreply.github.com>
2021-03-04 16:18:35 +0200

    Lesson 1 . name validation

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,16 +1,22 @@
-/**
- * Эту функцию трогать не нужно
- */
+
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  // ваш код...
+  if (!name) {
+    return false;
+  }
+
+  if (name.length < 4) {
+    return false;
+  }
+
+  if (name.indexOf(" ") !== -1) {
+    return false;
+  }
+
+  return true;
 }
 
 function sayHello() {
@@ -22,3 +28,4 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+sayHello()
