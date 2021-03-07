e11ca67ff0f52dd0b399bee760fd1ba9ae46434a
Lurker10 <Lurker10@users.noreply.github.com>
2021-03-07 15:27:17 +0200

    Lesson 2 . Task 1, 2, 3.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  let key;
+  for (key in salaries) {
+    if (Number.isNaN(salaries[key]) ||
+      salaries[key] == Number.POSITIVE_INFINITY ||
+      salaries[key] == Number.NEGATIVE_INFINITY) {
+    }
+    else if (typeof salaries[key] == "number") {
+
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
