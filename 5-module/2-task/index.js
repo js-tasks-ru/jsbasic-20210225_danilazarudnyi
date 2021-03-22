c64961d146268526f05aa2fe993f5598363bbcdb
Lurker10 <Lurker10@users.noreply.github.com>
2021-03-22 10:15:50 +0200

    module 5. 1,2 -task fixed

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,12 +2,7 @@ function toggleText() {
   const button = document.querySelector(".toggle-text-button");
   const text = document.getElementById('text');
 
-  button.onclick = function () {
-    if (!button) 
-    {
-      return
-    } 
+  button.onclick = () => {
       text.hidden = !text.hidden;
-    
   }
 }
